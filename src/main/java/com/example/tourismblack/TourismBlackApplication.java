2de57b85e02974406dbb451cda656d238436aa69
sx-9871 <linw@dtsphere.com>
2026-01-06 09:37:50 +0800

    初始化SpringBoot项目

---
+++ src/main/java/com/example/tourismblack/TourismBlackApplication.java
@@ -0,0 +1,13 @@
+package com.example.tourismblack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourismBlackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TourismBlackApplication.class, args);
+    }
+
+}
